471ae56369543d1115ec6e417a46fb0641c4d725
jesscianyquist <jessicanyquist3@gmail.com>
2016-07-18 12:54:50 +0200

    initial setup

---
+++ bucket.js
@@ -0,0 +1,32 @@
+var app = angular.module('bucketApp',['ngRoute', 'firebase']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/home', {
+		controller: 'HomeCtrl',
+		templateUrl: 'templates/home.html',
+	}),
+	$routeProvider.when('/signup', {
+		controller: 'SignUpCtrl',
+		templateUrl: 'templates/signup.html',
+	}),
+	$routeProvider.when('/login', {
+		controller: 'LogInCtrl',
+		templateUrl: 'templates/login.html',
+	}),
+	$routeProvider.when('/profile', {
+		controller: 'ProfileCtrl',
+		templateUrl: 'templates/profile.html',
+	}),
+
+app.controller('HomeCtrl', function($scope){
+
+});
+app.controller('SignUpCtrl', function($scope){
+
+});
+app.controller('LogInCtrl', function($scope){
+
+});
+app.controller('ProfileCtrl', function($scope){
+
+});
