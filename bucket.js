c6e274a7b0072f27dcd44baf70a79c49764ccff2
Rachel <Rachel@Rachels-MacBook-Pro-3.local>
2016-07-19 09:00:25 +0200

    Tuesday Morning for Steph

--- bucket.js
+++ bucket.js
@@ -2,6 +2,17 @@ var app = angular.module('bucketApp',['ngRoute', 'firebase']);
 app.factory('facebookService', function() {
 
 });
+
+app.run(["$rootScope", "$location", function($rootScope, $location) {
+  $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+    // We can catch the error thrown when the $requireSignIn promise is rejected
+    // and redirect the user back to the home page
+    if (error === "AUTH_REQUIRED") {
+       $location.path("/login");
+   }
+});
+}]);
+
 app.config(function($routeProvider) {
 	$routeProvider.when('/', {
 		controller: 'HomeCtrl',
@@ -15,13 +26,31 @@ app.config(function($routeProvider) {
 		controller: 'LogInCtrl',
 		templateUrl: 'templates/logon.html',
 	})
-	$routeProvider.when('/profile/', {
+	$routeProvider.when('/profile/:profileId', {
 		controller: 'ProfileCtrl',
 		templateUrl: 'templates/profile.html',
+		 resolve: {
+          // controller will not be loaded until $requireSignIn resolves
+          // Auth refers to our $firebaseAuth wrapper in the example above
+          "currentAuth": function($firebaseAuth) {
+                // $requireSignIn returns a promise so the resolve waits for it to complete
+                // If the promise is rejected, it will throw a $stateChangeError (see above)
+                return $firebaseAuth().$requireSignIn();
+            }
+        }
 	})
 	$routeProvider.when('/list/:listId', {
 		controller: 'ListCtrl',
 		templateUrl: 'templates/list.html',
+		 resolve: {
+          // controller will not be loaded until $requireSignIn resolves
+          // Auth refers to our $firebaseAuth wrapper in the example above
+          "currentAuth": function($firebaseAuth) {
+                // $requireSignIn returns a promise so the resolve waits for it to complete
+                // If the promise is rejected, it will throw a $stateChangeError (see above)
+                return $firebaseAuth().$requireSignIn();
+            }
+        }
 	})
 });
 
@@ -43,7 +72,7 @@ app.controller('NavCtrl', function($scope, $firebaseObject, $firebaseArray){
 	};
 
 });
-app.controller('SignUpCtrl', function($scope, $firebaseAuth, $firebaseObject){
+app.controller('SignUpCtrl', function($scope, $firebaseAuth, $firebaseObject, $location){
 	$scope.signUp = function(){
 		console.log($scope.name);
 		console.log($scope.password);
@@ -60,27 +89,52 @@ app.controller('SignUpCtrl', function($scope, $firebaseAuth, $firebaseObject){
 			$scope.user.email = $scope.email;
 			$scope.user.$save();
 			$scope.isSuccess= true;	
-			window.location.assign('#/list');   
+			// window.location.assign('#/list');
+			$location.path("/");   
 		}).catch(function(error) {
 			console.error("Error: ", error);
 			$scope.error = error;
-		});
-	}
+		}); 
+	} 	
 });
-app.controller('LogInCtrl', function($scope){
+
+app.controller('LogInCtrl', function($scope, $firebaseAuth, $routeParams, $location, $firebaseObject){
+	$scope.error = "";
 	$scope.facebookLogIn = function(){
 		console.log("log in with facebook");
 	}
+	
+	$scope.login = function() {
+		console.log($scope.email);
+		console.log($scope.password);
+	
+	$scope.authObj = $firebaseAuth();
 
+    $scope.authObj.$signInWithEmailAndPassword($scope.email, $scope.password)
+    .then(function(firebaseUser) {
+        console.log("Signed in as:", firebaseUser.uid);
+        $location.path("/");
+    }).catch(function(error) {
+        console.error("Authentication failed:", error);
+        $scope.errorMessage= error.message;
+    });
+}
 });
-app.controller('ProfileCtrl', function($scope, $firebaseArray){
+
+app.controller('ProfileCtrl', function($scope, $firebaseArray, $firebaseAuth, $routeParams, currentAuth){
 		console.log("create new list pop up");
+	currentAuth.uid = $routeParams.profileId;
 	var ref = firebase.database().ref().child('lists');
+	$scope.authObj = $firebaseAuth();
+	console.log(currentAuth);
+	console.log(currentAuth.uid);
+
 	$scope.lists = $firebaseArray(ref);
 		$scope.newList = function(){
 	$scope.lists.$add({
 		'title': $scope.title,
 		'description': $scope.description,
+		'user': $scope.currUser,
 	})
 	
 	};
@@ -90,23 +144,25 @@ app.controller('ProfileCtrl', function($scope, $firebaseArray){
 
 app.controller('ListCtrl', function($scope, $routeParams, $firebaseObject,$firebaseArray){
 	console.log($routeParams);
-	var number = $routeParams.listId;
-	console.log(number);
-	var ref= firebase.database().ref().child('lists').child(number);
+	var list_Id = $routeParams.listId;
+	console.log(list_Id);
+	var ref= firebase.database().ref().child('lists').child(list_Id);
 	$scope.list = $firebaseObject(ref);
 	console.log($scope.list);
 	$scope.createEvent = function(){
-		var ref= firebase.database().ref().child('lists').child(number).child('events');
+		var ref= firebase.database().ref().child('lists').child(list_Id).child('events');
 		var events = $firebaseArray(ref);
 		events.$add({
 			'title': $scope.eventName,
 			'created_at': Date.now(),
+			'isCompleted': false,
 		})
 		$scope.eventName='';
 
 	};
 	$scope.completed = function(){
 		console.log("task completed!");
+
 	}
 
 
